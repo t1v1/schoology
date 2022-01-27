dcf19556f52fee77d090ee6c30b4deab63adc735
t1v1 <team@thavma.club>
2022-01-26 21:52:23 -0800

    fix(content.js): use new `assessments` API

--- content.js
+++ content.js
@@ -8,18 +8,20 @@ browser.runtime.onMessage.addListener(async (message) => {
     });
     questions.push({ question, answers });
   });
-  const res = await fetch('http://localhost:3000/questions', {
+  const res = await fetch('http://localhost:3000/assessments', {
     method: 'post',
-    body: JSON.stringify(questions),
+    body: JSON.stringify({ questions }),
     headers: { 'Content-Type': 'application/json' },
   });
   const { id } = await res.json();
   window.setInterval(async () => {
-    const res = await fetch(`http://localhost:3000/answers?id=${id}`);
-    const answers = await res.json();
+    const res = await fetch(`http://localhost:3000/assessments/${id}`);
+    const { questions } = await res.json();
     const div = document.createElement('div');
     div.className = 't1v1';
-    div.innerText = answers.map((ans, idx) => `${idx + 1}. ${ans}`).join(' | ');
+    div.innerText = questions
+      .map((q, idx) => `${idx + 1}. (${q.answer + 1}) ${q.answers[q.answer]}`)
+      .join(' | ');
     document.body.appendChild(div);
   }, 1000);
 });
