3ef091ffd9adae23b55d5ad3bde5589066e0e22a
t1v1 <team@thavma.club>
2022-01-27 20:17:17 -0800

    wip: finish assessment beta mvp

--- background.js
+++ background.js
@@ -1,5 +1,28 @@
+const url = 'https://thavma.club';
 browser.browserAction.onClicked.addListener(() => {
+  console.log('Browser action clicked...');
   browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    console.log('Sending message...');
     browser.tabs.sendMessage(tabs[0].id, {});
   });
+});
+browser.runtime.onMessage.addListener(async ({ questions }) => {
+  console.log('Creating assessment...', { questions });
+  const res = await fetch(`${url}/api/assessments`, {
+    method: 'post',
+    body: JSON.stringify({ questions }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const { id } = await res.json();
+  console.log(`Created assessment (${id}).`);
+  window.setInterval(async () => {
+    console.log(`Fetching assessment (${id})...`);
+    const res = await fetch(`${url}/api/assessments/${id}`);
+    const { questions } = await res.json();
+    console.log(`Fetched assessment (${id}):`, { questions });
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+      console.log('Sending message...');
+      browser.tabs.sendMessage(tabs[0].id, { questions });
+    });
+  }, 1000);
 });
--- content.js
+++ content.js
@@ -1,27 +1,25 @@
 browser.runtime.onMessage.addListener(async (message) => {
-  const questions = [];
-  document.querySelectorAll('.item').forEach((item) => {
-    const question = item.querySelector('.lrn_question').textContent;
-    const answers = [];
-    item.querySelectorAll('.lrn-mcq-option .sr-only').forEach((ans) => {
-      answers.push(ans.textContent);
-    });
-    questions.push({ question, answers });
-  });
-  const res = await fetch('http://localhost:3000/assessments', {
-    method: 'post',
-    body: JSON.stringify({ questions }),
-    headers: { 'Content-Type': 'application/json' },
-  });
-  const { id } = await res.json();
-  window.setInterval(async () => {
-    const res = await fetch(`http://localhost:3000/assessments/${id}`);
-    const { questions } = await res.json();
-    const div = document.createElement('div');
-    div.className = 't1v1';
-    div.innerText = questions
+  if (message.questions) {
+    console.log('Showing questions...');
+    const div = document.getElementById('t1v1') || document.createElement('div');
+    div.id = 't1v1';
+    div.innerText = message.questions
       .map((q, idx) => `${idx + 1}. (${q.answer + 1}) ${q.answers[q.answer]}`)
       .join(' | ');
-    document.body.appendChild(div);
-  }, 1000);
+    if (!document.body.contains(div)) document.body.appendChild(div);
+    console.log('Showed questions:', message.questions);
+  } else {
+    console.log('Getting questions...');
+    const questions = [];
+    document.querySelectorAll('.item').forEach((item) => {
+      const question = item.querySelector('.lrn_question').textContent;
+      const answers = [];
+      item.querySelectorAll('.lrn-mcq-option .sr-only').forEach((ans) => {
+        answers.push(ans.textContent);
+      });
+      questions.push({ question, answers });
+    });
+    browser.runtime.sendMessage({ questions });
+    console.log('Got questions:', questions);
+  }
 });
